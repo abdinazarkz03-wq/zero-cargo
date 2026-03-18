25e370169fad11fc40cd57e600301561be2fb34f
abdinazarkz03-wq <abdinazar_k_z03@icloud.com>
2026-03-18 21:31:57 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-import threading
 import asyncio
-import os
 from webapp import app
-from bot import bot_start
+from bot import get_dispatcher_instance, get_bot_instance
+import threading
+import os
 
-def run_webapp():
+def run_flask():
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
+    app.run(host="0.0.0.0", port=port)
+
+async def main():
+    dp = get_dispatcher_instance()
+    bot = get_bot_instance()
+    # Запускаем Flask в отдельном потоке
+    threading.Thread(target=run_flask, daemon=True).start()
+    # Запускаем бота
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    threading.Thread(target=run_webapp, daemon=True).start()
-    asyncio.run(bot_start())
+    asyncio.run(main())
