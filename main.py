b2e0172d04a58478baad6eeb854fac9c7820eb71
abdinazarkz03-wq <abdinazar_k_z03@icloud.com>
2026-03-18 19:54:04 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
 import threading
 import asyncio
 import os
-import logging
 from webapp import app
-
-logging.basicConfig(level=logging.INFO)
+from bot import bot_start
 
 def run_webapp():
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
 
-async def run_bot():
-    from bot import main as bot_main
-    await bot_main()
-
 if __name__ == "__main__":
-    webapp_thread = threading.Thread(target=run_webapp, daemon=True)
-    webapp_thread.start()
-    asyncio.run(run_bot())
+    threading.Thread(target=run_webapp, daemon=True).start()
+    asyncio.run(bot_start())
