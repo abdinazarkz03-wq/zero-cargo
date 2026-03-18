1a17fc7adf1424caa28422580ce917bf61376ece
abdinazarkz03-wq <abdinazar_k_z03@icloud.com>
2026-03-19 02:05:55 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
+import os
 import asyncio
 import threading
-import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from webapp import app
 from bot import dp, bot
 
+
 def run_flask():
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, use_reloader=False)
+
 
 async def main():
-    # Запуск Flask в отдельном потоке
     threading.Thread(target=run_flask, daemon=True).start()
-    # Запуск Telegram бота
     print("🚀 Система ZERO CARGO запущена и готова к работе!")
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     try:
